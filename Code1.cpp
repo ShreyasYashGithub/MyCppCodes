318b3a949a9991b1b43067b350c9b6c5f1519cd4
Shreyas Rama <shreyasrama03@gmail.com>
2024-05-17 22:48:06 +0530

    Add files via upload

---
+++ Code1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+ 
+    int n = 5, rows, columns;
+    
+    for (rows = 1; rows <= n; rows++) {
+        
+        for (columns = n; columns > rows; columns--) {
+            cout << " ";
+        }
+        
+        cout << "*";
+       
+        for (columns = 1; columns < (rows - 1) * 2;
+             columns++) {
+            cout << " ";
+        }
+        if (rows == 1) {
+            cout << "\n";
+        }
+        else {
+            cout << "*\n";
+        }
+    }
+   
+    for (rows = n - 1; rows >= 1; rows--) {
+       
+        for (columns = n; columns > rows; columns--) {
+            cout << " ";
+        }
+      
+        cout << "*";
+        for (columns = 1; columns < (rows - 1) * 2;
+             columns++) {
+            cout << " ";
+        }
+        if (rows == 1) {
+            cout << "\n";
+        }
+        else {
+            cout << "*\n";
+        }
+    }
+    return 0;
+}
